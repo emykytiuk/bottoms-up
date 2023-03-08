a391a9eac2b6211251128f837f805eeb72cbfd45
emykytiuk <ethan.mykytiuk@strategyzer.com>
2023-03-08 13:20:32 -0700

    Updating codegen

--- client/codegen.ts
+++ client/codegen.ts
@@ -4,7 +4,7 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 const config: CodegenConfig = {
   overwrite: true,
   schema: './src/graphql/schema.graphql',
-  documents: "./src/graphql/**/*.graphql", ///**/*.graphql",
+  documents: "./src/graphql/**/*.graphql",
   generates: {
     "./src/graphql/types/": {
       preset: "client",
