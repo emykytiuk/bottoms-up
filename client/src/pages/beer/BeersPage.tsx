fd8a4773e129222b3aa05b4768d8946a56a4d425
emykytiuk <ethan.mykytiuk@strategyzer.com>
2023-03-23 14:43:26 -0600

    Fixing import

--- client/src/pages/beer/BeersPage.tsx
+++ client/src/pages/beer/BeersPage.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { useQuery } from "@apollo/client";
-import { BeerList } from "../../components/beer/BeerList";
+
 import { Loading } from "../../components/common/Loading";
 import { GetAllBeersDocument } from "../../graphql/types/graphql";
+import { BeerList } from "../../components/Beer/BeerList";
 
 export const BeersPage = () => {
   const { data, loading } = useQuery(GetAllBeersDocument);
