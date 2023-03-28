a5161ce09d4e11c5490c5000d9cb33c0cf55daf3
emykytiuk <85198222+emykytiuk@users.noreply.github.com>
2023-03-28 11:49:27 -0600

    Removing unused files (#5)

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,4 @@ Rails.application.routes.draw do
   if Rails.env.development?
     mount GraphqlPlayground::Rails::Engine, at: "/graphql_playground", graphql_path: "graphql"
   end
-
-  namespace :api do
-    namespace :v1 do
-      resources :breweries
-      resources :beers
-      resources :styles
-    end
-  end
 end
